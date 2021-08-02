6ed03c34639ae593a58999557dad3f165133188a
alghubari2017 <alghubari0772017@gmail.com>
2021-08-02 15:01:17 +0300

    add statelesswidget and appbar+action+background+icon+center

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  print("ali ggg");
-  var $a = "naje";
-  pr($a);
+  runApp(myApp());
 }
 
-void pr($a) {
-  print($a);
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Home",
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Home"),
+            backgroundColor: Colors.cyan,
+            actions: <Widget>[
+              IconButton(onPressed: () {}, icon: Icon(Icons.account_circle))
+            ],
+            centerTitle: true,
+          ),
+        ));
+  }
 }
