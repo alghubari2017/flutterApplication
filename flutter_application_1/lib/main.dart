20213b56759a3a893652c3d448fa6a4842daea00
alghubari2017 <alghubari0772017@gmail.com>
2021-08-02 15:40:41 +0300

    drawer-enddrawer--search

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -11,14 +11,17 @@ class myApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: "Home",
         home: Scaffold(
-          appBar: AppBar(
-            title: Text("Home"),
-            backgroundColor: Colors.cyan,
-            actions: <Widget>[
-              IconButton(onPressed: () {}, icon: Icon(Icons.account_circle))
-            ],
-            centerTitle: true,
-          ),
-        ));
+            appBar: AppBar(
+              title: Text("Home"),
+              backgroundColor: Colors.cyan,
+              actions: <Widget>[
+                // IconButton(onPressed: () {}, icon: Icon(Icons.search))
+              ],
+              centerTitle: true,
+              elevation: 1,
+              /* leading: IconButton(
+                  icon: Icon(Icons.ac_unit_sharp), onPressed: () {})),*/
+            ),
+            drawer: Drawer()));
   }
 }
