ba8d414d0b2d525f5cad2703f9cd23d664f4aa35
alghubari2017 <alghubari0772017@gmail.com>
2021-08-02 17:43:19 +0300

    UserAccountsDrawerHeade,currentacountPicture,boxdecoration

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -8,20 +8,37 @@ class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "Home",
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text("Home"),
-              backgroundColor: Colors.cyan,
-              actions: <Widget>[
-                // IconButton(onPressed: () {}, icon: Icon(Icons.search))
-              ],
-              centerTitle: true,
-              elevation: 1,
-              /* leading: IconButton(
-                  icon: Icon(Icons.ac_unit_sharp), onPressed: () {})),*/
-            ),
-            drawer: Drawer()));
+      debugShowCheckedModeBanner: false,
+      title: "Home",
+      home: Scaffold(
+        appBar: AppBar(
+            title: Text("Home"),
+            backgroundColor: Colors.cyan,
+            actions: <Widget>[
+              //IconButton(onPressed: () {}, icon: Icon(Icons.search))
+            ],
+            centerTitle: true,
+            elevation: 1,
+            leading: IconButton(icon: Icon(Icons.search), onPressed: () {})),
+        endDrawer: Drawer(
+            child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("ali alghubari"),
+              accountEmail: Text("alghubari20@gmail.com"),
+              currentAccountPicture: CircleAvatar(child: Icon(Icons.person)),
+              decoration: BoxDecoration(
+                color: Colors.red,
+                image: DecorationImage(
+                    image: NetworkImage(
+                        "https://modo3.com/thumbs/fit630x300/130760/1480157681/%D9%85%D8%AF%D9%8A%D9%86%D8%A9_%D8%A5%D8%A8_%D8%A7%D9%84%D8%AE%D8%B6%D8%B1%D8%A7%D8%A1.jpg"),
+                    fit: BoxFit.fill),
+              ),
+            )
+          ],
+        )),
+      ),
+      //drawer: Drawer()
+    );
   }
 }
